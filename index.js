755b953aba623e92ac6a9d1bc118e5d6e0797719
John <jgicheru325@gmail.com>
2024-04-07 00:05:05 +0300

    Objects lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+let employee = {
+    name: "John",
+    streetAddress: '334',
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+      ...employee,
+      [key]: value 
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const { [key]: deletedProperty, ...updatedEmployee } = employee;
+    return updatedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    if (employee.hasOwnProperty(key)) {
+        delete employee[key];
+    }
+    return employee;
+}
